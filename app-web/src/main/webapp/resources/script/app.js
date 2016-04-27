2a3e0ecb7e9a9a1299bc528010326a50d1896038
完蛋先生 <Administrator@PC-20150917PGXT>
2016-04-27 18:04:48 +0800

    增加flex布局

---
+++ app-web/src/main/webapp/resources/script/app.js
@@ -0,0 +1,24 @@
+var myApp = angular.module("myApp", []);
+myApp.controller("SomeController", function($scope) {
+
+});
+
+myApp
+		.directive(
+				'sidebox',
+				function() {
+					return {
+						restrict : 'EA',
+						scope : {
+							title : '@'
+						},
+						transclude : true,
+						template : '<div class="sidebox">\
+	<div class="content">\
+	<h2 class="header">{{ title }}</h2>\
+	<span class="content" ng-transclude>\
+	</span>\
+	</div>\
+	</div>'
+					};
+				});
