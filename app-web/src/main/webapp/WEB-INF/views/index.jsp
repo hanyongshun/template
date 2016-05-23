19a48de9b69be62ad4983f27fc5ccc7ecaf3c987
完蛋先生 <Administrator@PC-20150917PGXT>
2016-05-23 17:39:54 +0800

    js css 压缩

---
+++ app-web/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@include file="./common/common.jsp" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>测试</title>
+
+<script type="text/javascript">
+document.write("<script type='text/javascript' src='${contextPath}/resources/script/app.js'></scr"+"ipt>");
+</script>
+</head>
+<body>
+撒打发斯蒂芬 
+</body>
+</html>
--- app-web/src/main/webapp/resources/script/app.js
+++ app-web/src/main/webapp/resources/script/app.js
@@ -1,26 +1,2 @@
-var myApp = angular.module("myApp", ['ngRoute','simple']).config(['$routeProvider',function($routeProvider){
-	$routeProvider.when("/",{
-		templateUrl:'home',
-		controller:function($scope){
-			console.info("go to home" + $scope.name);
-		},
-		resolve:{
-			"data":["$http",function($http){
-				return{
-					name:"wjm"
-				}
-			}]
-		}
-	}).when("/list",{
-		templateUrl:'list',
-		controller:function($scope){
-			console.info("go to list");
-		}
-	}).when("/add",{
-		templateUrl:'add'
-	}).otherwise({
-		redirectTo:"/"
-	});
-}]);
-
-
+alert("1");
+alert("2");
